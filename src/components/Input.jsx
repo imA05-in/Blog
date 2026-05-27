ef302979ae58ad30a092dcb57e17aadc17153101
Abhishek <abhishek29112005@gmail.com>
2026-05-27 11:09:43 +0530

    Input Component

---
+++ src/components/Input.jsx
@@ -0,0 +1,9 @@
+import { ID } from "appwrite";
+
+export default function Input({type="text",label,ref,...props}){
+ return(
+    <div className="border rounded p-1 px-2 flex w-full justify-center items-center">
+    <input type={type} id={ID.unique()} {...props} ref={ref} className={`focus:outline-none`} placeholder={label}/>
+    </div>
+ )
+}
