ee01cf0139f0f12ef7c65d5181101361653ba1ba
Abhishek <abhishek29112005@gmail.com>
2026-05-27 11:12:43 +0530

    remove Logout

--- src/pages/indexP.js
+++ src/pages/indexP.js
@@ -2,6 +2,5 @@ import HomeP from "./HomeP";
 import LoginP from "./LoginP";
 import SingUpP from "./SignUpP";
 import AddPostP from "./AddPostP";
-import LogoutP from "./LogoutP";
 import AllPosts from "./AllPost";
-export {HomeP,LoginP, SingUpP, AddPostP, LogoutP, AllPosts}
+export {HomeP,LoginP, SingUpP, AddPostP, AllPosts}
