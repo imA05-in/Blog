5e6f30648d1877fa2e50690701d87f5447367701
Abhishek <abhishek29112005@gmail.com>
2026-05-27 11:14:49 +0530

    update navbar ui

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {createBrowserRouter, RouterProvider} from "react-router"
-import {HomeP,SingUpP,LoginP, AddPostP, LogoutP, AllPosts} from "./pages/indexP.js"
+import {HomeP,SingUpP,LoginP, AddPostP,AllPosts} from "./pages/indexP.js"
 import AuthLayout from "./components/AuthLayout.jsx"
+import { Provider } from 'react-redux'
+import { store } from './store/store.js'
 
 const router = createBrowserRouter([
   {
@@ -35,16 +37,15 @@ const router = createBrowserRouter([
         path:"add-post",
         element:<AddPostP/>
       },
-      {path:"logout",
-        element:<LogoutP/>
-      }
     ]
   }
 ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <RouterProvider router={router}/>
     {/* <App /> */}
+    </Provider>
   </StrictMode>,
 )
