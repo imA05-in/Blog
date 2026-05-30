2688e67bb12bc1bdc9c47b289c5f2547c0a72e15
Abhishek <abhishek29112005@gmail.com>
2026-05-30 13:21:13 +0530

    post component

---
+++ src/pages/Post.jsx
@@ -0,0 +1,46 @@
+import { useNavigate, useParams } from "react-router";
+import appwriteService from "../appwrite/Config";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Button } from "../components";
+export default function Post() {
+  const { slug } = useParams();
+  const [post, setPost] = useState("");
+ const navigate = useNavigate()
+  useEffect(() => {
+    appwriteService
+      .getRow(slug)
+      .then((row) => (setPost(row), console.log(row)))
+      .catch((error) => console.log("Post: ", error));
+  }, []);
+
+  const [file, setFile] = useState("");
+  appwriteService
+    .getfilePreview(post.ImageId)
+    .then((img) => setFile(img))
+    .catch((error) => console.log("PostCard :: getFilePreview: ", error));
+
+  const userData = useSelector((state) => state.auth.userData);
+
+  const isAuthor = userData.$id === post.userId ? true : false;
+
+  async function delte() {
+    appwriteService.deleteRow(post.$id).then(()=>navigate("/all-posts"))
+  }
+
+  return (
+    <div className="p-2 rounded">
+      <img src={file} alt={post.title} className="rounded" />
+      <div className="flex justify-between items-baseline">
+        <div>
+          <h2 className="font-bold text-2xl">{post.title}</h2>
+          <p className="text-gray-100">{`@${post.userName}`}</p>
+        </div>
+        {isAuthor && <div className="flex gap-2">
+            <Button label="edit"/>
+            <Button label="delete" clssName={`bg-red-500`} type="" onclick={delte}/>
+            </div>}
+      </div>
+    </div>
+  );
+}
