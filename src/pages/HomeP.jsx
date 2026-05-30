9406d7cfc7026b327948ab30a55e2c34e049bba8
Abhishek <abhishek29112005@gmail.com>
2026-05-30 13:26:14 +0530

    Home component

--- src/pages/HomeP.jsx
+++ src/pages/HomeP.jsx
@@ -1,8 +1,21 @@
-export default function HomeP()
-{
-    return (
-        <div>
-            Home Page
-        </div>
-    )
+import appwriteService from "../appwrite/Config";
+import { PostCard } from "../components";
+import { useState } from "react";
+export default function HomeP() {
+  const [posts, setPosts] = useState([]);
+
+  appwriteService
+    .listRows()
+    .then((post) => setPosts(post.rows))
+    .catch((error) => console.log("Homep: ", error));
+
+  return <div className="flex flex-col md:flex-row md:justify-center items-center gap-4 py-2 flex-wrap">
+    {
+        posts.map((post)=>(
+            <div key={post.title}>
+                <PostCard post={post}/>
+            </div>
+        ))
+    }
+  </div>;
 }
