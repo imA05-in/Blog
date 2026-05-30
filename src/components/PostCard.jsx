a173845961fc5c48ef9769124cf747fe63d1aec7
Abhishek <abhishek29112005@gmail.com>
2026-05-30 13:20:25 +0530

    PostCard component

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router";
+import appwriteService from "../appwrite/Config";
+import { useState } from "react";
+
+export default function PostCard({ post }) {
+  const [file, setFile] = useState("");
+
+  appwriteService
+    .getfilePreview(post.ImageId)
+    .then((img) => setFile(img))
+    .catch((error) => console.log("PostCard :: getFilePreview: ", error));
+
+  return (
+    <Link to={`/post/${post.$id}`}>
+      <div className="w-50 flex flex-col justify-center  gap-1 p-2 h-80 bg-gray-800 rounded-2xl border">
+        <img src={file} alt=""  className="rounded"/>
+        <h2 className="font-bold text-2xl">{post.title}</h2>
+        <p className="text-gray-100">{`@${post.userName}`}</p>
+      </div>
+    </Link>
+  );
+}
